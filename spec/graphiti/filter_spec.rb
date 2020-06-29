7deb4702eea1d268b13d922ab2f7e17a11d2ecc2
Zach Schneider <zach@schneider.dev>
2020-06-29 12:15:57 -0500

    Add a couple of filter specs

---
+++ spec/graphiti/filter_spec.rb
@@ -0,0 +1,55 @@
+RSpec.describe Graphiti::GraphQL do
+  it "filters top-level employees by name" do
+    Employee.create!(first_name: "Joe", last_name: "Smith")
+    Employee.create!(first_name: "Joe", last_name: "Thomas")
+    Employee.create!(first_name: "Jack", last_name: "Thomas")
+
+    query = <<~QUERY
+      query {
+        employees(firstNameEq: "Joe") {
+          firstName
+          lastName
+        }
+      }
+    QUERY
+    expect(query).to respond_with(
+      "employees" => [
+        { "firstName" => "Joe", "lastName" => "Smith" },
+        { "firstName" => "Joe", "lastName" => "Thomas" }
+      ]
+    )
+  end
+
+  it "filters nested positions by title" do
+    employee = Employee.create!(first_name: "Joe", last_name: "Smith")
+    department1 = Department.create!(name: "Paper Management")
+    Position.create!(department: department1, employee: employee, title: "Assistant to Regional Manager")
+    department2 = Department.create!(name: "Pencil Pushing")
+    Position.create!(department: department2, employee: employee, title: "Regional Manager")
+
+    query = <<~QUERY
+      query {
+        employees {
+          firstName
+          lastName
+          positions(titleEq: "Assistant to Regional Manager") {
+            title
+          }
+        }
+      }
+    QUERY
+    expect(query).to respond_with(
+      "employees" => [
+        {
+          "firstName" => "Joe",
+          "lastName" => "Smith",
+          "positions" => [
+            {
+              "title" => "Assistant to Regional Manager"
+            }
+          ]
+        }
+      ]
+    )
+  end
+end
